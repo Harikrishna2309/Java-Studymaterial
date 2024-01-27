f0144a993444e54a09addaae56e545708f285602
hari krishna <harikrishna.m23sep@gmail.com>
2024-01-27 23:32:14 +0530

    collections updated

--- src/zpractice/ReverseString.java
+++ src/zpractice/ReverseString.java
@@ -18,6 +18,30 @@ public class ReverseString {
 			System.out.print(stack.pop()+" ");
 		}
 		System.out.println();
+		
+		Queue <Character> q1=new ArrayDeque <Character>();
+		for(int i=0;i<num;i++) {
+			q1.add(input.charAt(i));
+		}
+		//System.out.println(q1.poll());
+		Iterator <Character>i1=q1.iterator();
+		System.out.println("by arrayqueue");
+		while(i1.hasNext()) {
+			System.out.print(i1.next()+" ");
+		}
+		System.out.println();
+		Queue <Character> q2=new PriorityQueue <Character>();
+		for(int i=0;i<num;i++) {
+			q2.add(input.charAt(i));
+		}
+		//System.out.println(q1.poll());
+		Iterator <Character>i2=q2.iterator();
+		System.out.println("by priorityqueue");
+		while(i2.hasNext()) {
+			System.out.print(i2.next()+" ");
+		}
+		System.out.println();
+		
 		System.out.println("by array list");
 		
 		ArrayList<Character>array=new ArrayList<Character>();
@@ -27,12 +51,14 @@ public class ReverseString {
 		for(int i=num-1;i>=0;i--) {
 			System.out.print(array.get(i)+" ");
 		}
+		System.out.println("size= "+array.size());
 		System.out.println();
 		System.out.println("by array");
 		char arr[]=new char[num];
 		for(int i=0;i<num;i++) {
 			arr[i]=input.charAt(i);
 		}
+		
 		for(int i=num-1;i>=0;i--) {
 			System.out.print(arr[i]+" ");
 		}
--- src/zpractice/Swap.java
+++ src/zpractice/Swap.java
@@ -1,5 +1,5 @@
 package zpractice;
-
+import java.util.*;
 import java.util.Arrays;
 
 public class Swap {
@@ -35,11 +35,43 @@ public class Swap {
 		System.out.println(s1);
 		s1.append(" M");
 		System.out.println(s1);*/
-		for(int i=0;i<1;i++) {
+		/*for(int i=0;i<1;i++) {
 			System.out.println(i+' ');
 			//ans 32 ASCII value of space is 32 because java concate charcter with number it change charcter to ascii
 			//if" "using double quotes its value is 0
-		}
+		}*/
+		
+		Queue<Integer>q1=new PriorityQueue<Integer>();
+		q1.add(50);
+		q1.add(1);
+		q1.add(55);
+		q1.add(45);
+		System.out.println(q1.poll());
+		System.out.println(q1.poll());
+		
+		Queue<Character>q2=new PriorityQueue<Character>();
+		q2.add('h');
+		q2.add('a');
+		q2.add('r');
+		q2.add('i');
+		
+		System.out.println(q2.poll());
+		System.out.println(q2.peek());
+		System.out.println(q2.poll());
+		System.out.println(q2.poll());
+		
+		
+		Deque<Character>q3=new ArrayDeque<Character>();
+		q3.push('h');
+		q3.add('a');
+		q3.add('r');
+		q3.add('i');
+		System.out.println("Array");
+		System.out.println(q3.pop());
+		System.out.println(q3.poll());
+		System.out.println(q3.poll());
+		System.out.println(q3.poll());
+
 
 	}
 
