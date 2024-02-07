8505ed6a59313c857ad1ffd6231f94b486212429
hari krishna <harikrishna.m23sep@gmail.com>
2024-02-07 18:38:01 +0530

    map updated

--- src/interviewProblems/pangramCheck.java
+++ src/interviewProblems/pangramCheck.java
@@ -3,24 +3,28 @@ import java.util.*;
 import java.lang.*;
 public class pangramCheck {
 
-	public static <Integer> void main(String[] args) {
+	public static void main(String[] args) {
 		
-		try {
-			String s="The quick brown fox jumps over the lazy dog";
-			s=s.replaceAll("\\s","");
-			int n;
-			char []arr=s.toCharArray();
-			Map<Character, Integer>hash=new HashMap<Character,Integer>();
-			
-			for(Character c:arr) {
-				hash.put(c, null);
-			}
-			int size=hash.size();
-			System.out.println(size);
-		}
-		catch(Exception e) {
-			System.out.println(e);
-		}
+//		try {
+//			String s="The quick brown fox jumps over the lazy dog";
+//			s=s.replaceAll("\\s","");
+//			int n;
+//			char []arr=s.toCharArray();
+//			Map<Character, Integer>hash=new HashMap<Character,Integer>();
+//			
+//			for(Character c:arr) {
+//				hash.put(c, null);
+//			}
+//			int size=hash.size();
+//			System.out.println(size);
+//		}
+//		catch(Exception e) {
+//			System.out.println(e);
+//		}
+		
+		
+		int a=5;
+		System.out.println(a);
 		
 		
 
