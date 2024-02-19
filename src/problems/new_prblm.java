e3cfe7a941f15a69bec9d9a18d3c368871debfb4
hari krishna <harikrishna.m23sep@gmail.com>
2024-02-19 22:44:32 +0530

    empty push

--- src/problems/new_prblm.java
+++ src/problems/new_prblm.java
@@ -3,7 +3,7 @@ package problems;
 public class new_prblm {
 
 	public static void main(String[] args) {
-		System.out.println("i will select in akkena animation tomorrow");
+		System.out.println("i will select in google");
 
 	}
 
